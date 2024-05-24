c8b235df975675f053a5992f746e4fb0ff186ee0
abhishekbhonde51 <abhishekbhonde_cs@jspmrscoe.edu.in>
2024-05-24 22:54:36 +0530

    handling api

--- lib/apiHandling.dart
+++ lib/apiHandling.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import "package:http/http.dart" as http;
 
@@ -12,11 +13,22 @@ class NewsApp extends StatefulWidget {
 }
 
 class _NewsAppState extends State<NewsApp> {
+  List<dynamic> productDetails = [];
   void newsDetails() async {
-    Uri url = Uri.parse(
-        'https://newsapi.org/v2/everything?q=tesla&from=2024-04-24&sortBy=publishedAt&apiKey=API_KEY');
-    http.Response response = await http.get(url);
-    log(response.body);
+    try {
+      Uri url = Uri.parse(
+          'https://newsapi.org/v2/everything?q=tesla&from=2024-04-24&sortBy=publishedAt&apiKey=c6f761505a47457eac563f53842e0642');
+      http.Response response = await http.get(url);
+      log(response.body);
+
+      var responseData = json.decode(response.body);
+
+      setState(() {
+        productDetails = responseData['articles'];
+      });
+    } catch (e) {
+      log("Failed in fetching data");
+    }
   }
 
   @override
@@ -26,17 +38,35 @@ class _NewsAppState extends State<NewsApp> {
         title: Text(
           "ShortsNews",
           style: GoogleFonts.quicksand(
-            fontSize: 20,
-            color: Colors.orange,
-          ),
+              fontSize: 25, color: Colors.orange, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.blue[200],
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Column(
-          children: [],
-        );
-      }),
+      body: ListView.builder(
+        itemCount: productDetails.length,
+        itemBuilder: (context, index) {
+          var article = productDetails[index];
+          var author = article['author'] ?? 'Anonymous';
+          var title = article['title'] ?? 'No title';
+          var description = article['description'] ?? 'No description';
+          var imageUrl = article['urlToImage'];
+          var publishedDate = article['publishedAt'];
+        
+
+          return Container(
+            child: Column(
+              children: [
+                Text(author),
+                Text(description),
+                Text(imageUrl),
+                Text(publishedDate)
+              ],
+              
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           newsDetails();
